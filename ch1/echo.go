9e7b914f609b5dc0a933529b2f7ce250eea6f8d7
Johann Diedrick <johann.diedrick@metmuseum.org>
2017-01-17 11:54:36 -0500

    first commit

---
+++ ch1/echo.go
@@ -0,0 +1,53 @@
+//prints command-line arguments
+package main
+
+/*
+func main() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
+*/
+
+/*
+func main() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+
+}
+*/
+
+/*
+func main() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
+*/
+
+/*
+//exercise 1.1
+func main() {
+	fmt.Println(strings.Join(os.Args[0:], " "))
+}
+*/
+
+/*
+//exercise 1.2
+func main() {
+	for i := 1; i < len(os.Args); i++ {
+		fmt.Println(i, os.Args[i])
+	}
+}
+*/
+
+//exercise 1.3
+func main() {
+	// not sure what to do yet...
+	return
+}
