7d58e08a67b436b4645a313db52e018866988523
Johann Diedrick <johann.diedrick@metmuseum.org>
2017-01-23 11:33:01 -0500

    simple echo server

---
+++ ch1/server1/server1.go
@@ -0,0 +1,18 @@
+//minimal "echo" web server
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler) // each request calls handler
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+//handler echos the path component of the requested url
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "URL.Path = %q \n", r.URL.Path)
+}
