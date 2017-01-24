c5f17095c7fb048d06295c7400330a229b2b45a7
Johann Diedrick <johann.diedrick@metmuseum.org>
2017-01-24 16:25:59 -0500

    added ftoc example

---
+++ ch2/ftoc/ftoc.go
@@ -0,0 +1,14 @@
+// prints two fahrenheit-t-celsius conversions
+package main
+
+import "fmt"
+
+func main() {
+	const freezingF, boilingF = 32.0, 212.0
+	fmt.Printf("%g degrees F = %g degrees C\n", freezingF, fToC(freezingF))
+	fmt.Printf("%g degrees F = %g degrees C\n", boilingF, fToC(boilingF))
+}
+
+func fToC(f float64) float64 {
+	return (f - 32) * 5 / 9
+}
